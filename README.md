bacf34c7f91a8e46e9d19d383f928a3bc6d8060a
Maciej Ciara <maciej.ciara@thomsonreuters.com>
2016-11-26 21:26:44 +0100

    Calculate loan based on user input

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
 Loan Calculator
 ===============
 
-React application calculating a cost of a mortage loan using Cicero Financial Solutions (CFS).
-
-
-
+React application calculating a cost of mortgage loan using Cicero Financial Solutions (CFS).
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br>
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+
+## Development
+
+The app uses CFS which is a resource from a different domain and that will be blocked by most browsers. 
+You might install a browser plugin to modify the Allow-Control-Allow-Origin header of the response:
+* Chrome: https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi/reviews?hl=en
+* Firefox: https://addons.mozilla.org/en-US/firefox/addon/cors-everywhere/
--- src/App.js
+++ src/App.js
@@ -1,16 +1,141 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class LoanParameters extends Component {
+  constructor() {
+    super();
+    this.handleAmountChange = this.handleAmountChange.bind(this);
+    this.handleRateChange = this.handleRateChange.bind(this);
+    this.handleTimeChange = this.handleTimeChange.bind(this);
+  }
+
+  handleAmountChange (event) {
+    this.props.onAmountChange(event.target.value);
+  }
+
+  handleRateChange (event) {
+    this.props.onRateChange(event.target.value);
+  }
+
+  handleTimeChange (event) {
+    this.props.onTimeChange(event.target.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <label>Loan amount
+          <input id="amount" type="text" onChange={this.handleAmountChange}/>
+        </label>
+        <label>Down payment years
+          <input id="years" type="text" onChange={this.handleTimeChange}/>
+        </label>
+        <label>Interest rate
+          <input id="rate" type="text" onChange={this.handleRateChange}/>
+        </label>    
+      </div>
+    );
+  }
+}
+
+class CalculatedPayment extends Component {
+  render() {
+    if (this.props.payment !== null) {
+      return (
+        <div>
+          <h2>Monthly Payment</h2>
+          <h2>
+            {Math.round(this.props.payment)}
+          </h2>
+        </div>
+      );
+    } else {
+      return (<div/>);
+    }
+  }
+}
+
+CalculatedPayment.propTypes = {
+  payment: React.PropTypes.number
+};
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      payment: null,
+      loanAmount : null,
+      paymentTime: null,
+      interestRate: null
+    };
+    this.handleCalculateClick = this.handleCalculateClick.bind(this);
+    this.handleAmountChange = this.handleAmountChange.bind(this);
+    this.handleInterestRateChange = this.handleInterestRateChange.bind(this);
+    this.handlePaymentTimeChange = this.handlePaymentTimeChange.bind(this);
+  }
+
+  handleCalculateClick() {
+    var today = new Date();
+    var raisingYear = today.getFullYear();
+    var raisingMonth = today.getMonth() + 2; // + 2 to get the next month
+    if (raisingMonth > 12) { // if the next month is January, a year has to be updated
+      raisingMonth = 1;
+      ++raisingYear;
+    }
+
+    if (this.state.loanAmount != null && this.state.interestRate != null && this.state.paymentTime != null) {
+      fetch("https://cfs-ws-itera.cicero.no/cfp/6/ws/rest/calculator/calculateLoan?loanRaisingMonth=" + raisingMonth + 
+        "&loanRaisingYear=" + raisingYear + 
+        "&principalAmount=" + this.state.loanAmount + 
+        "&annualNominalInterestRate=" + this.state.interestRate +
+        "&totalNumberOfPayments="+ this.state.paymentTime * 12)
+      .then((response) => response.json())
+      .then((responseJson) => { 
+        var payment = 0;
+        if (responseJson.amortizationSchedule && responseJson.amortizationSchedule.length > 0) {
+          payment = responseJson.amortizationSchedule[0].payment;
+        }
+            
+        this.setState({ payment });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    } else {
+      // TODO Please fill in all fields
+    }
+  }  
+
+  handleAmountChange(loanAmount) {
+    this.setState({ loanAmount });
+  }
+
+  handleInterestRateChange(interestRate) {
+    this.setState({ interestRate });
+  }
+
+  handlePaymentTimeChange(paymentTime) {
+    this.setState({ paymentTime });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Loan Calculator</h2>
+          <h1>Loan Calculator</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <LoanParameters 
+            onAmountChange={this.handleAmountChange}
+            onRateChange={this.handleInterestRateChange}
+            onTimeChange={this.handlePaymentTimeChange} />          
+        </div>
+        <div>
+        <button onClick={this.handleCalculateClick}>Calculate</button>
+        </div>
+        <div className="App-content">
+          <CalculatedPayment payment={this.state.payment}/>          
+        </div>
       </div>
     );
   }
